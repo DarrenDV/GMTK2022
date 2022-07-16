d72cc72721112a7958f3520e17bdcf564714ba61
jellemiddendorf <Jelle.middendorf@hotmail.com>
2022-07-16 16:05:05 +0200

    Main menu done (fuck anchors)
    allignment is still broken needs a fix

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //Application.LoadLevel(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        Debug.Log("Credits");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
